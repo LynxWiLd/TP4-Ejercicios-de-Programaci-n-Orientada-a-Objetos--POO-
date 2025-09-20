c0fe8b592abe0a66c81a022bee44e6cf0dc7afc6
Vera_Facundo <facundovera647@gmail.com>
2025-09-20 18:41:59 -0300

    Ejercicio01 terminado

--- js/ejercicio01.js
+++ js/ejercicio01.js
@@ -5,3 +5,30 @@ Output:
 objeto.encender(); auto encendido
 objeto.apagar();    El auto se apagó
 */
+
+function onOff(opcion) {
+  switch (opcion) {
+    case "1": {
+      console.log(auto.encender);
+      break;
+    }
+    case "2": {
+      console.log(auto.apagar);
+      break;
+    }
+    default:
+      console.log("Opcion Invalida");
+      break;
+  }
+}
+
+var auto = {
+  color: "Azul",
+  marca: "Toyota",
+  modelo: "2015",
+  encender: "Auto encendido",
+  apagar: "El auto se apagó",
+};
+
+let opcion = prompt("Opcion(1)Encender Auto/Opcion(2)Apagar Auto");
+onOff(opcion);
